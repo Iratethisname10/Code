23f78c12fbca4b3f467d79ae264e95c6bfdd182f
IRateThisName10 <115908538+Iratethisname10@users.noreply.github.com>
2024-06-05 12:32:26 +0800

    Update test.lua

--- scripts/test.lua
+++ scripts/test.lua
@@ -1,10 +1,2 @@
 local library = loadstring(game:HttpGet('https://raw.githubusercontent.com/Iratethisname10/Code/main/aztup-ui/ui-lib.lua'))()
-
-local a = library:AddTab('test Tab')
-local a1, a2 = a:AddColumn(), a:AddColumn()
-
-local v = a1:AddSection('Test Section')
-
-v:AddToggle({text = 'Test Button'})
-
 library:Init()
